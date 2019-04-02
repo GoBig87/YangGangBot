6879d1f0c563cc990296b5119724480d6ed9108d
GoBig87 <nobody5390@yahoo.com>
2019-04-01 22:59:05 -0700

    Adding twitter handle python file
    list of twitter handles added and start of twitter crawler

--- TwitterCrawler.py
+++ TwitterCrawler.py
@@ -1,4 +1,25 @@
+import tweepy
+import Twitter_Handles
+
 
 class TwitterCrawler():
     def __init__(self):
-        pass
+        consumer_key    = "s3qSyonaSiQZMXE5lM5XhNlgM"
+        consumer_secret = "BtGe8Loj1uP7j3TAKSR7WkurNS8AX7d8XghvU8ia9N5HXVVqlZ"
+        access_token    = "iEfRYop97bnbpkP8gtaQ9thNXmSKTC"
+        access_token_secret = "EvxGLeGQnb895AlOxeLvtrBierVJPJcv5XkB4SOLs37q"
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+
+        self.api = tweepy.API(auth)
+
+    def searchTweets(self):
+
+        public_tweets = self.api.home_timeline()
+        for tweet in public_tweets:
+            print
+            tweet.text
+
+
+if __name__ == "__main__":
+    tc = TwitterCrawler()
